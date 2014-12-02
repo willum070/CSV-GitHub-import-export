2d4baee0ba63bc86210f957c98a9c4b339b139d7
William French <willum070@yahoo.com>
2014-12-02 11:36:49 -0800

    will's fixes

--- csv_issues_to_github.rb
+++ csv_issues_to_github.rb
@@ -1,44 +1,54 @@
 #!/usr/local/bin/ruby
 
 # See the README for how to use this.
+# You may also need to mess with the cacert.pem certificate:
+# https://gist.github.com/fnichol/867550
+# http://railsapps.github.io/openssl-certificate-verify-failed.html
+
+# The first line of the CSV file MUST contain column headers.
+# This code is set up to process the following:
+# title,description,assignee_username,label1,label2,label3
+# Values must reflect valid values as defined in GitHub.
+# For example, assignee_username must ALWAYS contain a valid username.
 
 require 'rubygems'
 require 'octokit'
 require 'faraday'
 require 'csv'
+require 'open-uri'
 
 # BEGIN INTERACTIVE SECTION
 # Comment out this section (from here down to where the end is marked) if you want to use this interactively
 
-puts "Username:"
-username = gets.chomp  
-if username == ""
-	abort("You need to supply a username. Thank you, come again.")
-end
-
-puts "Password:"
-password = gets.chomp  
-if password == ""
-	abort("You need to supply a password. Thank you, come again.")
-end
-
-puts "Path for the CSV file you want to use?"
-input_file = gets.chomp  
-if input_file == ""
-	abort("You need to supply a CSV file. Thank you, come again.")
-end
-
-puts "Organization?"
-org = gets.chomp  
-if org == ""
-	abort("You need to supply an organization. Thank you, come again.")
-end
-
-puts "Repository?"
-repo = gets.chomp  
-if repo == ""
-	abort("You need to supply a repository. Thank you, come again.")
-end
+# puts "Username:"
+# username = gets.chomp
+# if username == ""
+# 	abort("You need to supply a username. Thank you, come again.")
+# end
+#
+# puts "Password:"
+# password = gets.chomp
+# if password == ""
+# 	abort("You need to supply a password. Thank you, come again.")
+# end
+#
+# puts "Path for the CSV file you want to use?"
+# input_file = gets.chomp
+# if input_file == ""
+# 	abort("You need to supply a CSV file. Thank you, come again.")
+# end
+#
+# puts "Organization?"
+# org = gets.chomp
+# if org == ""
+# 	abort("You need to supply an organization. Thank you, come again.")
+# end
+#
+# puts "Repository?"
+# repo = gets.chomp
+# if repo == ""
+# 	abort("You need to supply a repository. Thank you, come again.")
+# end
 
 # END INTERACTIVE SECTION
 
@@ -46,25 +56,36 @@ end
 # BEGIN HARD-CODED SECTION
 # Un-comment out this section (from here down to where the end is marked) if you want to use this without any interaction
 # All of these need to be filled out in order for it to work
-=begin
-input_file = ""
+
+input_file = "OSSbugsnew.csv"
 username = ""
 password = ""
-org = "" 
-repo = ""
-=end  # END HARD-CODED SECTION
+org = "MSRConnections"
+repo = "OpenWWT-Web"
 
 org_repo = org + "/" + repo
 
-client = Octokit::Client.new(:login => username, :password => password)
+# For some reason this line does not work.
+# client = Octokit::Client.new(:login => username, :password => password)
+
+# This one does, however!
+# Provide authentication credentials
+Octokit.configure do |c|
+  c.login = username
+  c.password = password
+end
+
+client = Octokit::Client.new()
+
+#puts Octokit.user
 
 csv_text = File.read(input_file)
 csv = CSV.parse(csv_text, :headers => true)
 
 csv.each do |row|
 	client.create_issue(org_repo, row['title'], row['description'], options = {
-		:assignee => row['assignee_username'], 
+		:assignee => row['assignee_username'],
+        #:milestone => row['milestone'],
 		:labels => [row['label1'],row['label2'],row['label3']]})  #Add or remove label columns here.
 	puts "Imported issue:  #{row['title']}"
 end
-
--- github_issues_to_csv.rb
+++ github_issues_to_csv.rb
@@ -8,38 +8,38 @@ require 'date'
 # BEGIN INTERACTIVE SECTION
 # Comment out this section (from here down to where the end is marked) if you want to use this interactively
 
-puts "Username:"
-USERNAME = gets.chomp  
-if USERNAME == ""
-	abort("You need to supply a username. Thank you, come again.")
-end
-
-puts "Password:"
-PASSWORD = gets.chomp  
-if PASSWORD == ""
-	abort("You need to supply a password. Thank you, come again.")
-end
-
-puts "Name of the file you want to create? (.csv will be appended automatically)"
-OUTPUT_FILE = gets.chomp  
-if OUTPUT_FILE == ""
-	abort("You need to supply a CSV file. Thank you, come again.")
-end
-
-puts "Organization?"
-ORG = gets.chomp  
-if ORG == ""
-	abort("You need to supply an organization. Thank you, come again.")
-end
-
-puts "Repository?"
-REPO = gets.chomp  
-if REPO == ""
-	abort("You need to supply a repository. Thank you, come again.")
-end
-
-puts "Do you want just one Milestone? If so, enter it now. Leave this blank to get the entire repo."
-TARGET_MILESTONE = gets.chomp  
+# puts "Username:"
+# USERNAME = gets.chomp
+# if USERNAME == ""
+# 	abort("You need to supply a username. Thank you, come again.")
+# end
+#
+# puts "Password:"
+# PASSWORD = gets.chomp
+# if PASSWORD == ""
+# 	abort("You need to supply a password. Thank you, come again.")
+# end
+#
+# puts "Name of the file you want to create? (.csv will be appended automatically)"
+# OUTPUT_FILE = gets.chomp
+# if OUTPUT_FILE == ""
+# 	abort("You need to supply a CSV file. Thank you, come again.")
+# end
+#
+# puts "Organization?"
+# ORG = gets.chomp
+# if ORG == ""
+# 	abort("You need to supply an organization. Thank you, come again.")
+# end
+#
+# puts "Repository?"
+# REPO = gets.chomp
+# if REPO == ""
+# 	abort("You need to supply a repository. Thank you, come again.")
+# end
+#
+# puts "Do you want just one Milestone? If so, enter it now. Leave this blank to get the entire repo."
+# TARGET_MILESTONE = gets.chomp
 
 # END INTERACTIVE SECTION
 
@@ -47,24 +47,24 @@ TARGET_MILESTONE = gets.chomp
 # BEGIN HARD-CODED SECTION
 # Un-comment out this section (from here down to where the end is marked) if you want to use this without any interaction
 # All of these need to be filled out in order for it to work
-=begin
-OUTPUT_FILE = ""
+
+OUTPUT_FILE = "OpenWWT-Issues"
 USERNAME = ""   # Put your GitHub username inside the quotes
-PASSWORD = ""   # Put your GitHub password inside the quotes 
-ORG = ""        # Put your organization (or username if you have no org) name here
-REPO = ""       # Put the repository name here
+PASSWORD = ""   # Put your GitHub password inside the quotes
+ORG = "MSRConnections"
+REPO = "OpenWWT-Web"
 # Want to only get a single milestone? Put the milestone name in here:
 TARGET_MILESTONE="" # keep this equal to "" if you want all milestones
-=end  # END HARD-CODED SECTION
+
 
 
 # Your local timezone offset to convert times
-TIMEZONE_OFFSET="-4"
- 
+TIMEZONE_OFFSET="-8"
+
 client = Octokit::Client.new(:login => USERNAME, :password => PASSWORD)
- 
+
 csv = CSV.new(File.open(File.dirname(__FILE__) + "/" + OUTPUT_FILE + ".csv", 'w'))
- 
+
 puts "Initialising CSV file..."
 #CSV Headers
 header = [
@@ -81,7 +81,7 @@ header = [
 ]
 
 csv << header
- 
+
 puts "Getting issues from Github..."
 temp_issues = []
 issues = []
@@ -98,7 +98,7 @@ begin
 	temp_issues = client.list_issues("#{ORG}/#{REPO}", :state => "open", :page => page)
 	issues = issues + temp_issues;
 end while not temp_issues.empty?
- 
+
 puts "Processing #{issues.size} issues..."
 issues.each do |issue|
 
@@ -106,29 +106,30 @@ issues.each do |issue|
 	label = issue['labels'] || "None"
 	if (label != "None")
 		label.each do |item|
-    		labels += item['name'] + " " 
-    	end	
+    		labels += item['name'] + " "
+    	end
 	end
 
 	assignee = ""
 	assignee = issue['assignee'] || "None"
 	if (assignee != "None")
 		assignee = assignee['login']
-	end	
+	end
 
 	milestone = issue['milestone'] || "None"
 	if (milestone != "None")
 		milestone = milestone['title']
 	end
- 
+
 	if ((TARGET_MILESTONE == "") || (milestone == TARGET_MILESTONE))
     # Needs to match the header order above, date format are based on Jira default
     row = [
       issue['number'],
       issue['title'],
       issue['body'],
-      DateTime.parse(issue['created_at']).new_offset(TIMEZONE_OFFSET).strftime("%d/%b/%y %l:%M %p"),
-      DateTime.parse(issue['updated_at']).new_offset(TIMEZONE_OFFSET).strftime("%d/%b/%y %l:%M %p"),
+	  # added to_s so that the datetime string is properly created.
+      DateTime.parse(issue['created_at'].to_s).new_offset(TIMEZONE_OFFSET).strftime("%d/%b/%y %l:%M %p"),
+      DateTime.parse(issue['updated_at'].to_s).new_offset(TIMEZONE_OFFSET).strftime("%d/%b/%y %l:%M %p"),
       labels,
       milestone,
       issue['state'],
